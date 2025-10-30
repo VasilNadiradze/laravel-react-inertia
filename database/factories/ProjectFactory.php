f4541423cd3825941c5c32928a8d2de383941073
vaso <vaso@vasos-MacBook-Pro.local>
2025-10-30 18:07:08 +0400

    - საინიციალზიზაციო კომიტი

---
+++ database/factories/ProjectFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProjectFactory extends Factory
+{
+    public function definition(): array
+    {
+        // pick a random user (or first user)
+        $user = User::first() ?? User::factory()->create();
+
+        return [
+            'name' => fake()->sentence(),
+            'description' => fake()->realText(),
+            'due_date' => fake()->dateTimeBetween('now', '+1 year'),
+            'status' => fake()->randomElement(['pending', 'in_progress', 'completed']),
+            'image_path' => fake()->imageUrl(),
+            'created_by' => $user->id,
+            'updated_by' => $user->id,
+        ];
+    }
+}
---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Task>
+ */
+class TaskFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {     
+        // pick a random user (or first user)
+        $user = User::first() ?? User::factory()->create();
+
+        return [
+            'name' => fake()->sentence(),
+            'description' => fake()->realText(),
+            'due_date' => fake()->dateTimeBetween('now', '+1 year'),
+            'status' => fake()->randomElement(['pending','in_progress','completed']),
+            'priority' => fake()->randomElement(['low','medium','high']),
+            'image_path' => fake()->imageUrl(),
+            'assigned_user_id' => $user->id,
+            'created_by' => $user->id,
+            'updated_by' => $user->id,
+        ];
+    }
+}
